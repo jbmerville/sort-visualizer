520c3599d4a98f22c630f6235916314d5dd2e87d
jbmerville <jbmer@my.yorku.ca>
2020-03-28 01:28:40 +0100

    Initial set up of the project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const App = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
